97b413b3ae86f1f1cd1dfbb2827845f86e99cad4
Bart Massey <bart.massey@gmail.com>
2024-09-27 15:05:35 -0700

    added outcomes

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,33 @@ enum Tile {
     X,
 }
 
+#[derive(Debug, Clone, Copy)]
+enum Outcome {
+    P1Win,
+    P2Win,
+    Draw,
+}
+
+impl std::fmt::Display for Outcome {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        let outcome = match self {
+            Outcome::P1Win => "Player 1 wins",
+            Outcome::P2Win => "Player 2 wins",
+            Outcome::Draw => "Draw",
+        };
+        write!(f, "{}", outcome)
+    }
+}
+
 type Board = [[Option<Tile>; 4]; 4];
 
 type Bag = HashBag<Tile>;
 
 fn main() {
-    let mut _board: Board = Default::default();
-    let mut _bag: Bag = [(Tile::F, 5), (Tile::O, 6), (Tile::X, 5)]
+    let mut board: Board = Default::default();
+    let mut bag: Bag = [(Tile::F, 5), (Tile::O, 6), (Tile::X, 5)]
         .into_iter()
         .collect();
-    //let outcome = play(&mut board, &mut bag);
+    let outcome = play(&mut board, &mut bag);
+    println!("{:?}", outcome);
 }
